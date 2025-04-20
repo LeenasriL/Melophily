b4b420ffdb21eddb2028ad129f04b994694d74d1
LeenasriL <lleenasri20@gmail.com>
2025-04-20 18:52:35 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,8 @@
+document.addEventListener("click", function() { 
+    var hiddenText = document.getElementById("text1"); 
+    hiddenText.style.display = "block"; 
+  }); 
+  document.addEventListener("click", function() { 
+    var hiddenText = document.getElementById("text2"); 
+    hiddenText.style.display = "block"; 
+  }); 
